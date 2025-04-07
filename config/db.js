c7e1eb8ddd30422b87bed229e3ed87936fb8e6d9
HarshPandey2111 <106433638+HarshPandey2111@users.noreply.github.com>
2025-04-07 17:31:40 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Replace with your MongoDB connection string if needed
+
     const conn = await mongoose.connect('mongodb://localhost:27017/node_auth_app', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
